e946c40a0b2b23e510c38fd2229920686876ad63
SeongBeom <dhtjdqja66@naver.com>
2026-05-17 23:29:27 +0900

    내일 고칠거

---
+++ Practice/Practice1/practice1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main() {
+START:
+	printf("메뉴를 선택하세요. \n");
+	printf("[A] 산술연산 (나눗셈) \n");
+	printf("[B] 비트 연산 (AND)\n");
+	printf("[Q] 프로그램 종료\n");
+
+	char menu = 0;
+	int nInput1 = 0, nInput2 = 0, result = 0;
+	menu = getchar();
+
+	switch (menu)
+	{
+	case 'A' :
+	case 'a' :
+		scanf_s("%d%d%*c", &nInput1, &nInput2);
+		if (nInput2 == 0) {
+			printf("0으로 나눌 수 없습니다.");
+			goto START;
+			break;
+		}
+		else {
+			printf("%.2f", (float)(nInput1 / nInput2));
+			goto START;
+			break;
+		}
+
+	case 'B' :
+	case 'b' :
+		scanf_s("%d%d%*c", &nInput1, &nInput2);
+		result = nInput1 & nInput2;
+		printf("두 정수 비트 AND값은 : %d, 메모리 크기는 각각 : %zu, %zu", result, sizeof(nInput1), sizeof(nInput2));
+		goto START;
+		break;
+
+	case 'Q' :
+	case 'q' :
+		printf("프로그램은 종료합니다.");
+		break;
+
+	default :
+		printf("잘못된 입력입니다.");
+		goto START;
+
+
+	}
+
+}
